eea48998dc5d58471409a99ef2dd01d205788272
Nate River <river177169@proton.me>
2025-03-05 01:49:19 +0530

    added lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+#![feature(f128)]
+
+pub mod c_types;
+#[allow(dead_code)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+pub mod bindings;
+
+
